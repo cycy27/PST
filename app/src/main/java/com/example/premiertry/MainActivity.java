dca43966881d2c0c10f27b1d9d507187a8e35e13
cycy <oub.cyr@gmail.com>
2022-04-19 16:19:54 +0200

    changes made

--- app/src/main/java/com/example/premiertry/MainActivity.java
+++ app/src/main/java/com/example/premiertry/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.premiertry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/////////////////////////////////////////////////////////////
+        ImageButton btn = (ImageButton)findViewById(R.id.checkup_button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Check_up.class));
+                finish();
+
+            }
+        });
+ //////////////////////////////////////////////////////////////
+
+
+
     }
 }
