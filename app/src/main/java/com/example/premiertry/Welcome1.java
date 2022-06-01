8192030d853e72e3f0ba461ec304538fcddd3ebc
nesrinelagraa <34574221+nesrinelagraa@users.noreply.github.com>
2022-06-01 18:04:08 +0200

    Revert "Revert "Database added and changes made ""

---
+++ app/src/main/java/com/example/premiertry/Welcome1.java
@@ -0,0 +1,49 @@
+package com.example.premiertry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Welcome1 extends AppCompatActivity {
+    private Button loginbutton;
+    private Button registerbutton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome1);
+
+        loginbutton = (Button) findViewById(R.id.loginbutton);
+        loginbutton.setOnClickListener(new View.OnClickListener (){
+            @Override
+            public void onClick(View v){
+                openSignin();
+            }
+        });
+
+        registerbutton = (Button) findViewById(R.id.registerbutton);
+        registerbutton.setOnClickListener(new View.OnClickListener (){
+            @Override
+            public void onClick(View v){
+                opensingup();
+            }
+        });
+
+
+    }
+
+    public void opensingup(){
+        Intent intent = new Intent(this, Singup.class);
+        startActivity(intent);
+    }
+    public void openSignin(){
+        Intent intent = new Intent(this, Signin.class);
+        startActivity(intent);
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/premiertry/Welcome3.java
@@ -0,0 +1,32 @@
+package com.example.premiertry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class Welcome3 extends AppCompatActivity {
+
+    private ImageView Buttonnextw3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome3);
+
+        Buttonnextw3 = (ImageView) findViewById(R.id.Buttonnextw3);
+        Buttonnextw3.setOnClickListener(new View.OnClickListener (){
+            @Override
+            public void onClick(View v){
+                openMainActivity();
+            }
+        });
+    }
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
