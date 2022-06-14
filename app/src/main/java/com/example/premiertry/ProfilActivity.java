6f1e301270cca6307b2f44eb98bf38b201e09403
Nesrine Lagraa <nesrina@Nesrines-MacBook-Pro.local>
2022-06-14 12:38:18 +0200

    profil changed

--- app/src/main/java/com/example/premiertry/ProfilActivity.java
+++ app/src/main/java/com/example/premiertry/ProfilActivity.java
@@ -2,6 +2,7 @@ package com.example.premiertry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -56,17 +57,14 @@ public class ProfilActivity extends AppCompatActivity {
                     profil= new Profil(-1,profilAdress.getText().toString(),profilEmail.getText().toString(),profilNumber.getText().toString(),Integer.parseInt(profilAge.getText().toString()),
                             Integer.parseInt(profilWeight.getText().toString()),Integer.parseInt(profilHeight.getText().toString()),profilSexButton.getText().toString(),profilSmokingButton.getText().toString(),
                             profilContactName.getText().toString(),profilContactNumber.getText().toString());
+                    Toast.makeText(ProfilActivity.this, "Added to Database!",Toast.LENGTH_SHORT).show();
 
                 }catch(Exception e){
                     Toast.makeText(ProfilActivity.this, "error",Toast.LENGTH_SHORT).show();
                     profil= new Profil(-1,"error","error","error",-1,-1,-1,"error","error","error","error");
 
                 }
-                boolean done= dataBaseHelper.addOneProfil(profil);
-
-
-                Toast.makeText(ProfilActivity.this,
-                        profilSexButton.getText(), Toast.LENGTH_SHORT).show();
+                //boolean done= dataBaseHelper.addOneProfil(profil);
 
 
             }
@@ -76,10 +74,15 @@ public class ProfilActivity extends AppCompatActivity {
         profilHelpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                openHelp();
             }
         });
 
+    }
+
+    public void openHelp(){
+        Intent intent = new Intent(this, HelpActivity.class);
+        startActivity(intent);
     }
 
 }
